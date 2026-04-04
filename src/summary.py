8ba63d6ec90334fc5bdf51ed00a8b499cc5f1c3f
sudeepisstupid123 <66318414+sudeepisstupid123@users.noreply.github.com>
2026-04-04 18:11:36 +0530

    added miss_case_table & miss_var_table fucntions

--- src/summary.py
+++ src/summary.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from utils import count_missing, validate_dataframe
+
+
 
 def miss_var_summary(df):
     """
@@ -14,8 +17,7 @@ def miss_var_summary(df):
     Summary table with missingness statistics per variable
     """
 
-    if not isinstance(df, pd.DataFrame):
-        raise ValueError("Input must be a pandas DataFrame.")
+    validate_dataframe(df)
     
 
     summary = pd.DataFrame({
@@ -31,5 +33,67 @@ def miss_var_summary(df):
         ascending=False
     ).reset_index(drop=True)
 
+def miss_var_table(df):
+    """
+    Create a frequency table of missing values for each variable in a pandas DataFrame.
 
-    
+    Parameters:
+    -----------
+    df: pandas DataFrame
+
+    Returns:
+    --------
+    pandas DataFrame
+        A table showing:
+        - n_miss_in_var: number of missing values in a variable
+        - n_vars: number of variables with that missing count
+        - pct_vars: percentage of total variables
+    """
+    validate_dataframe(df)
+    miss_counts = count_missing(df, axis=0)
+    table=(
+        miss_counts.value_counts().reset_index()
+    )
+    table.columns=["n_miss_in_var","n_vars"]
+
+    table["pct_vars"] = (
+        table["n_vars"] / len(df.columns) * 100
+    ).round(2)
+
+    return table.sort_values(
+        by="n_miss_in_var",
+        ascending=False
+    ).reset_index(drop=True)   
+
+
+
+def miss_case_table(df):
+    """
+    Create a frequency table of missing values for each case (row) in a pandas DataFrame.
+
+    Parameters:
+    -----------
+    df: pandas DataFrame
+
+    Returns:
+    pandas DataFrame
+        A table showing:
+        - n_miss_in_case: number of missing values in a row 
+        - n_cases: number of cases with that missing count
+        - pct_cases: percentage of total cases
+    """
+
+    validate_dataframe(df)
+
+    miss_counts = count_missing(df, axis=1)
+    table=(
+        miss_counts.value_counts().reset_index()
+    )
+    table.columns=["n_miss_in_case","n_cases"]
+
+    table["pct_cases"]=(
+        table["n_cases"]/len(df)*100
+    ).round(2)
+
+    return table.sort_values(by="n_miss_in_case", ascending=False).reset_index(drop=True)
+
