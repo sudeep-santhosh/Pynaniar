7f978f09e23c0a79e7c48c1526e664f003e44f91
sudeepisstupid123 <66318414+sudeepisstupid123@users.noreply.github.com>
2026-06-02 03:21:43 +0530

    created multiple validation functions for handelling errors

--- src/validation.py
+++ src/validation.py
@@ -26,3 +26,34 @@ def validate_dataframe(df):
     if df.columns.duplicated().any():
         warnings.warn("DataFrame contains duplicate column names.")
 
+def validate_search(search):
+
+    if search is None:
+        raise ValueError("'search' cannot be None.")
+
+    if isinstance(search, (list, tuple, set)) and len(search) == 0:
+        raise ValueError("'search' cannot be empty.")
+    
+def validate_column(df, column):
+
+    if len(column) == 0:
+        raise ValueError("Column name cannot be empty.")
+
+    if column not in df.columns:
+        raise ValueError(f"Column '{column}' does not exist in the DataFrame.")
+
+def validation_span_check(span):
+
+    if not isinstance(span, int):
+        raise TypeError(
+            "'span_every' must be an integer."
+        )
+    
+    if not isinstance(span, int) or span <= 0:
+        raise ValueError("'span_every' must be a positive integer.")
+    
+
+def validation_x(x):
+    if x is None:
+        raise ValueError("'x' cannot be None.")
+    
